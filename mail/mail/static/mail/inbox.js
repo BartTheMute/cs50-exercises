d63d0b093b6b464a68fedafffe349edf0398bcac
Bart, the Mute <bartvanpaassen@gmail.com>
2022-04-29 16:27:46 +0200

    added opening and reading emails

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector('#compose-form').addEventListener('submit', send_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_email);  
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function compose_email() {
 
   // Show compose view and hide other views
+  document.querySelector('#detail-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
@@ -21,11 +22,14 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  
+
 }
 
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
+  document.querySelector('#detail-view').style.display = 'none';
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
@@ -58,6 +62,7 @@ function load_mailbox(mailbox) {
       div.appendChild(timestampdiv);
       
       document.querySelector('#emails-view').appendChild(div);
+      div.addEventListener('click', () => load_mail(mail.id));
     });
   })
   .catch(error => {
@@ -88,4 +93,48 @@ function send_email(){
   });
 
   load_mailbox('sent')
+}
+
+function load_mail(mailId){
+  document.querySelector('#detail-view').style.display = 'block';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  fetch('/emails/' + mailId,{
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(mail => {
+    let div = document.createElement('div');
+
+    let senderdiv = document.createElement('div');
+    senderdiv.textContent = mail.sender;
+    div.appendChild(senderdiv);
+
+    let recdiv = document.createElement('div');
+    recdiv.textContent = mail.recipients;
+    div.appendChild(recdiv);
+
+    let subjectdiv = document.createElement('div');
+    subjectdiv.textContent = mail.subject;
+    div.appendChild(subjectdiv);
+
+    let timestampdiv = document.createElement('div');
+    timestampdiv.textContent = mail.timestamp;
+    div.appendChild(timestampdiv);
+
+    let bodydiv = document.createElement('div');
+    bodydiv.textContent = mail.body;
+    div.appendChild(bodydiv);
+    
+    document.querySelector('#detail-view').appendChild(div);  
+  })
+  .catch(error => console.log(error));
+
+  fetch('/emails/' + mailId, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: true
+    })
+  });
 }
